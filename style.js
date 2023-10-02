b5dbe4c2a18190f204f630077c353419d21a8bd1
Reynold Mantes <143402929+ReynoldMantes@users.noreply.github.com>
2023-10-02 04:16:01 -0700

    Add files via upload

---
+++ style.js
@@ -0,0 +1,9 @@
+var age = prompt('ENTER YOUR AGE');
+var remainingYears = 90 - age
+var daysInAYear = 365
+var weeksInAYear = 52
+var monthsInAYear = 12
+var x = remainingYears * daysInAYear
+var y = remainingYears * weeksInAYear
+var z = remainingYears * monthsInAYear
+alert ('In days, you have ' + x + ' left. In weeks, you have ' + y + ' left. In months, you have ' + z + ' left')
